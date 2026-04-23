b7b0ed505d76cc775748d3be2e73ed0c7f0ea352
Mudassir006 <mudassir.cuiatd@gmail.com>
2026-04-23 10:58:18 +0300

    [ADD] fix attendance sync issue with device

--- pr_attendance_device/__manifest__.py
+++ pr_attendance_device/__manifest__.py
@@ -2,7 +2,7 @@
     'name': "Biometric Attendance Machines Integration",
     'author': "Guess Who",
     'website': 'mudassir',
-    'version': '1.1.8',
+    'version': '1.1.9',
     'license': 'LGPL-3',
 
     # any module necessary for this one to work correctly
