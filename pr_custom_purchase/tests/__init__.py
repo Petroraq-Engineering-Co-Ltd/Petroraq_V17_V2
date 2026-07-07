816313c628b23c7621250572dfb01b0d685b7903
Mudassir006 <mudassir.cuiatd@gmail.com>
2026-07-07 09:49:23 +0300

    [ADD] cleaner PO approval and rejection workflow. set dept of budget in pr for people who dont have employees.

--- pr_custom_purchase/tests/__init__.py
+++ pr_custom_purchase/tests/__init__.py
@@ -1,4 +1,5 @@
 from . import test_purchase_requisition_product_defaults
+from . import test_purchase_requisition_department_fallback
 from . import test_purchase_order_rejection
 from . import test_cash_pr_voucher_budget
 from . import test_cash_pr_attachments
