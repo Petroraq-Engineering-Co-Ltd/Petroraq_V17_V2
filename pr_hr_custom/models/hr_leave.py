55bce62d82d4e588577b4ba13662b441875ae3b8
WaqassAlii <97884227+waqassalii@users.noreply.github.com>
2026-08-09 19:28:19 +0500

    [FIX] duplication error

--- pr_hr_custom/models/hr_leave.py
+++ pr_hr_custom/models/hr_leave.py
@@ -4,48 +4,6 @@ class HrLeave(models.Model):
     _inherit = 'hr.leave'
 
     @api.model
-    def get_employee_absentee_day_details(self, employee_id=None, duration='current_contract', date_from=False, date_to=False):
-        res = super().get_employee_absentee_day_details(
-            employee_id=employee_id,
-            duration=duration,
-            date_from=date_from,
-            date_to=date_to
-        )
-        emp_id = res.get('employee_id')
-        if not emp_id:
-            return res
-        d_from = fields.Date.from_string(res.get('date_from')) if res.get('date_from') else False
-        d_to = fields.Date.from_string(res.get('date_to')) if res.get('date_to') else False
-        domain = [
-            ('employee_id', '=', emp_id),
-            ('check_in', '!=', False),
-            ('check_out', '!=', False)
-        ]
-        if d_from:
-            domain.append(('check_in', '>=', d_from))
-        if d_to:
-            domain.append(('check_in', '<=', d_to))
-        attendances = self.env['hr.attendance'].sudo().search(domain)
-        missing_checkout_recs = attendances.filtered(lambda a: a.check_in == a.check_out)
-        new_rows = list(res.get('rows', []))
-        for att in missing_checkout_recs:
-            emp = att.employee_id
-            new_rows.append({
-                'date': att.check_in.date(),
-                'day_name': att.check_in.strftime('%A'),
-                'employee_id': emp.id,
-                'employee_code': getattr(emp, 'registration_number', False) or emp.barcode or '',
-                'department': emp.department_id.name if emp.department_id else '',
-                'job_position': emp.job_id.name if emp.job_id else '',
-                'check_in': fields.Datetime.to_string(att.check_in),
-                'reason': 'Missing Checkout',
-                'shift': emp.resource_calendar_id.name if emp.resource_calendar_id else '',
-            })
-        res['rows'] = new_rows
-        res['count'] = len(new_rows)
-        return res
-
-    @api.model
     def _get_employee_absentee_day_rows(self, employee, start, end):
         rows = super()._get_employee_absentee_day_rows(employee, start, end)
         if not employee or not start or not end:
