01c9e61ebff5fc52194f5406b10708d5e247f7fd
WaqassAlii <97884227+waqassalii@users.noreply.github.com>
2026-08-09 15:33:54 +0500

    [ADD] custom module [ADD] cron job for missing checkouts

---
+++ pr_hr_custom/__manifest__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "PetroRaq Customization",
+    'author': "WaqasAli",
+    'category': 'Uncategorized',
+    'version': '0.1',
+    'depends': ['base', 'hr_attendance'],
+    'data': [
+        'data/cron_missing_checkouts.xml',
+    ],
+}
