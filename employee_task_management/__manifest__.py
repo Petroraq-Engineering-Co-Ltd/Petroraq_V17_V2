fb1dc11538366d22da4a319e4abf4e021069ff29
Omer Saleem <omersaleem09@hotmail.com>
2026-08-31 12:25:19 +0500

    Fix domain

--- employee_task_management/__manifest__.py
+++ employee_task_management/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': 'Employee Task Management',
-    'version': '17.0.1.23.2',
+    'version': '17.0.1.23.3',
     'category': 'Human Resources',
     'summary': 'Employee Task List Approval Workflow',
     'description': """
