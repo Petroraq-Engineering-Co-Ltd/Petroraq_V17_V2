24895fbb8b160160d2a7de1fd9da19fc3f17ec9e
noorulmustafa <noor.ul.mustafa0102@gmail.com>
2026-09-08 13:58:50 +0300

    added library pypdf

--- pr_account/__manifest__.py
+++ pr_account/__manifest__.py
@@ -20,6 +20,9 @@
     "license": "LGPL-3",
     # any module necessary for this one to work correctly
     'depends': ['account_accountant', 'account_reports', 'de_hr_workspace', 'l10n_sa_edi'],
+    'external_dependencies': {
+    'python': ['pypdf'],
+    },
 
     # always loaded
     'data': [
